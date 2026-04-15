c20892b501bc2e5e45011642cd1ef1a529db5250
puntodewo.fadhillah <puntodewo.fadhillah@bluebirdgroup.com>
2026-04-15 17:35:29 +0700

    replace button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -123,17 +123,6 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <motion.div
-        className="absolute bottom-8 left-1/2 -translate-x-1/2"
-        animate={{ y: [0, 8, 0] }}
-        transition={{ repeat: Infinity, duration: 2 }}
-      >
-        <div className="w-6 h-10 rounded-full border-2 border-muted-foreground/30 flex justify-center pt-2">
-          <div className="w-1 h-2 rounded-full bg-muted-foreground/50" />
-        </div>
-      </motion.div>
-
       <ContactFormDialog open={contactOpen} onOpenChange={setContactOpen} />
     </section>
   );
